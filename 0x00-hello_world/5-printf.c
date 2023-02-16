38a50b63290f382c308b8cdb8669acba45f838bb
Tobbiloba <salauoluwatobiloba0@gmail.com>
2023-02-16 12:53:12 -0800

    done

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
- * main - A program that uses printf
- * return: 0
+ * main - Entry of the program
+ * Return: Always returns (0)
  */
+
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n");
-return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
