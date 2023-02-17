d01653fa1d104507d0e46d1e403f12a32051c170
Tobbiloba <salauoluwatobiloba0@gmail.com>
2023-02-17 08:44:34 -0800

    done

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Function
+ * Return: result
+ */
+int main(void)
+{
+
+	int num = 0;
+
+	do {
+		printf("%d", num);
+		num++;
+	} while (num < 10);
+	printf("\n");
+	return (0);
+}
