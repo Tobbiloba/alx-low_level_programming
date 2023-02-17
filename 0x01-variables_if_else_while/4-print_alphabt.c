59fdf5a5be8f599f0d0f233033fb16f1cc5603c7
Tobbiloba <salauoluwatobiloba0@gmail.com>
2023-02-17 05:53:29 -0800

    done~

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Function
+ * Return: result
+ */
+int main(void)
+{
+
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		if (c == 'e' || c == 'q')
+		{
+		}
+		else
+		{
+			putchar(c);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
