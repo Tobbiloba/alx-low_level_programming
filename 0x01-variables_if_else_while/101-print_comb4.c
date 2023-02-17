9879708b6a4b77c87a0d33eaffeb0c64e7a9ffde
Tobbiloba <salauoluwatobiloba0@gmail.com>
2023-02-17 11:56:42 -0800

    done

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+/**
+ * main - function
+ * Return: result
+ */
+
+int main(void)
+{
+
+	int hund = 0;
+
+	int tens = 1;
+
+	int unit = 2;
+
+	while (hund < 8)
+	{
+		int i = tens;
+
+		while (tens <= 8 && i <= 8)
+		{
+			int j = unit;
+
+			while (unit <= 9 && j <= 9)
+			{
+				putchar(hund + '0');
+				putchar(i + '0');
+				putchar(j + '0');
+
+					if (hund < 7)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				j++;
+			}
+			i++;
+		}
+		unit++;
+		tens++;
+		hund++;
+	}
+	return (0);
+}
