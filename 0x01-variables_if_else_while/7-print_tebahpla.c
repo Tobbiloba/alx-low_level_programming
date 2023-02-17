3dab50b3ab7949f67c7bee5bfa3ca51c6f427333
Tobbiloba <salauoluwatobiloba0@gmail.com>
2023-02-17 08:57:41 -0800

    done

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Function
+ * Return: result
+ */
+int main(void)
+{
+	char c = 'z';
+
+	while (c >= 'a')
+	{
+		putchar(c);
+		c--;
+	}
+	putchar('\n');
+	return (0);
+}
