c06cea46bb40f07adea7ce38f922d3b386a7fa29
Tobbiloba <salauoluwatobiloba0@gmail.com>
2023-02-21 09:39:49 -0800

    done

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,9 +1,12 @@
-#include "main.h"
-
+#include <unistd.h>
 /**
- * main - Function
- * Return: bla bla bla
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ * main - prints bla bla bla
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
+#include "main.h"
 void print_alphabet_x10(void)
 {
 	int i = 0;
