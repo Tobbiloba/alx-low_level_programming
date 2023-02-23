e90cbbb538a1487f6e66553e2d01e7293ed1a68e
Tobbiloba <salauoluwatobiloba0@gmail.com>
2023-02-23 11:04:44 -0800

    done

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_line - It prints an underscore n times
+ * @n: is the amount ot line you want to be printed
+ *
+ * Return: result
+ */
+void print_line(int n)
+{
+	int i = 0;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		while (i < n)
+		{
+			_putchar('_');
+			i++;
+		}
+	}
+	_putchar('\n');
+}
