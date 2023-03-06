6dbe4c51fdeb6a4061f46df4c59f07bd6aca5540
Tobbiloba <salauoluwatobiloba0@gmail.com>
2023-03-06 04:05:35 -0800

    done

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strchr - returns the address of where the char is found in the string
+ * @s: The name of the string
+ * @c: The name of the char
+ * Return: Result
+ */
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	for (; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+			return (&s[i]);
+		i++;
+	}
+	return (0);
+}
