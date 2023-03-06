de72b904c06aca8aa5c8a1ae21ec9aff1d9306ee
Tobbiloba <salauoluwatobiloba0@gmail.com>
2023-03-06 03:43:09 -0800

    done 0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _memset - The nam of the function
+ * @s: The integer we are adding to
+ * @b: The integer we are adding fro
+ * @n: The number of integers we are adding
+ * Return: Result
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i = 0;
+
+	for (;n > 0; i++)
+	{
+		s[i] = b;
+		i++;
+		n--;
+	}
+	return (s);
+}
