356d33ccb14966ad10283cfbe4b296fd99167b52
Tobbiloba <salauoluwatobiloba0@gmail.com>
2023-03-14 04:55:08 -0700

    done

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * _strdup - The name of the function
+ * @str: The params
+ * Return: The result
+ */
+char *_strdup(char *str)
+{
+	int i, r;
+	char *string;
+
+	if (str == NULL)
+		return (NULL);
+
+	
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+
+	string = malloc(sizeof(char) * (i + 1));
+
+	if (string == NULL)
+		return (NULL);
+
+	for (r = 0; str[i]; r++)
+		string[i] = str[i];
+	return (string);
+}
